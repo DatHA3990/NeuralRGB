c1314c066cec679a638045476e88204bc784a4c7
DatHA3990 <dathayt@gmail.com>
2017-05-04 10:21:49 -0400

    lots of new stuff

--- NeuralRGB/NeuralRGB.cpp
+++ NeuralRGB/NeuralRGB.cpp
@@ -1,43 +1,55 @@
-#include "stdafx.h"''
+#include "stdafx.h"
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#include <fstream>
 #include <iostream>
+#include <map>
+#include <ios>
 #include <sstream>
 #include <string>
 #include <vector>
 
-using namespace cv;
-using namespace std;
+struct Color {
+	std::string colorName;
+	cv::Scalar bgr;
+	int tolerance = 0;
+};
 
 cv::Scalar getAvg(std::vector<cv::Scalar> imgData) {
 	cv::Scalar avg;
 	for (int i = 0; i < 3; i++)
 		avg[i] = 0;
 	int num = imgData.size();
-	for (int rgb = 0; rgb < 3; rgb++) {
-		for (int i = 0; i < num; i++) {
+	for (int rgb = 0; rgb < 3; rgb++)
+		for (int i = 0; i < num; i++)
 			avg[rgb] += imgData[i][rgb];
-		}
-	}
 	for (int i = 0; i < 3; i++)
 		avg[i] /= num;
 	return avg;
 }
 
-void training(cv::Scalar &avg) {
+void training(std::vector<Color> &color) {
+	std::ifstream file;
+	file.open("../TrainData/1/0.txt");
+	std::string colorName;
+	file >> colorName;
+	file.close();
 	std::vector<cv::Scalar> imgData;
-	for (int i = 0; i <= 6; i++) {
-		std::string fname = "../TrainData/" + std::to_string(i) + ".jpg";
-		cv::Mat image = imread(fname, IMREAD_COLOR);
+	for (int i = 1; i <= 10; i++) {
+		std::string fname = "../TrainData/1/" + std::to_string(i) + ".jpg";
+		cv::Mat image = cv::imread(fname, cv::IMREAD_COLOR);
 		cv::Scalar imgBgr = cv::mean(image);
 		imgData.push_back(imgBgr);
-		waitKey(1);
+		cv::waitKey(1);
 	}
-	avg = getAvg(imgData);
-	std::cout << avg << std::endl << std::endl;
+	Color currentColor;
+	currentColor.colorName = colorName;
+	currentColor.bgr = getAvg(imgData);
+	color.push_back(currentColor);
+	std::cout << color[0].colorName << " : " << color[0].bgr << std::endl << std::endl;
 }
 
 bool isInRange(cv::Scalar tested, cv::Scalar color, int tolerance) {
@@ -50,21 +62,22 @@ bool isInRange(cv::Scalar tested, cv::Scalar color, int tolerance) {
 	return true;
 }
 
-bool isColor(cv::Mat img, cv::Scalar colorBGR, int tolerance) {
-	cv::Scalar imgBGR = cv::mean(img);
-	std::cout << imgBGR << std::endl;
-	return isInRange(imgBGR, colorBGR, tolerance);
+bool isColor(cv::Mat img, cv::Scalar colorBgr, int tolerance) {
+	cv::Scalar imgBgr = cv::mean(img);
+	std::cout << imgBgr << std::endl;
+	return isInRange(imgBgr, colorBgr, tolerance);
 }
 
 bool getBoolInput() {
-	std::cout << "Was I correct? >>>";
+	std::cout << "Was I correct? 1:Yes 0:No >>>";
 	std::string x;
 	std::cin >> x;
 	if (x == "1")
 		return true;
-	if (x == "0")
+	else if (x == "0")
 		return false;
-	getBoolInput();
+	else
+		getBoolInput();
 }
 
 int getNewTolerance(int tolerance, bool attempt, bool isCorrect) {
@@ -75,30 +88,30 @@ int getNewTolerance(int tolerance, bool attempt, bool isCorrect) {
 	else if (attempt == false)
 		return tolerance + 10;
 	else
-		getNewTolerance;
+		return tolerance;
 }
 
-void test(cv::Scalar colorBGR, std::string fname, int &tolerance, std::string testedColorName, std::string colorName) {
-	cv::Mat image = imread(fname, IMREAD_COLOR);
-	bool isBlue = isColor(image, colorBGR, tolerance);
+void test(Color &color, std::string fname, std::string testedColorName) {
+	cv::Mat image = cv::imread(fname, cv::IMREAD_COLOR);
+	bool isBlue = isColor(image, color.bgr, color.tolerance);
 
-	std::string message = testedColorName + (isBlue ? " is " : " is not ") + colorName;
+	std::string message = testedColorName + (isBlue ? " is " : " is not ") + color.colorName;
 	std::cout << message << std::endl;
-
+	
 	bool correct = getBoolInput();
-	tolerance = getNewTolerance(tolerance, isBlue, correct);
-	std::cout << tolerance << std::endl << std::endl;
+	color.tolerance = getNewTolerance(color.tolerance, isBlue, correct);
+	std::cout << color.tolerance << std::endl << std::endl;
 }
 
 int main() {
-	cv::Scalar blue;
-	training(blue);
+	std::cout << "1.02" << std::endl << std::endl;
 
-	int tolerance = 0;
+	std::vector<Color> color;
+	training(color);
 
-	while (1) {
-		test(blue, "../TestData/blue.jpg", tolerance, "blue", "blue");
-		test(blue, "../TestData/orange.jpg", tolerance, "orange", "blue");
-		test(blue, "../TestData/purple.jpg", tolerance, "purple", "blue");
+	while (true) {
+		test(color[0], "../TestData/blue.jpg", "blue");
+		test(color[0], "../TestData/orange.jpg", "orange");
+		test(color[0], "../TestData/purple.jpg", "purple");
 	}
 }
